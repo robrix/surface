3b7e4ba162ca83f183c09ba03562eef01581e5ef
Rob Rix <rob.rix@me.com>
2017-01-29 11:39:17 -0500

    Define an Applicative instance for Result.

--- src/Data/Result.hs
+++ src/Data/Result.hs
@@ -3,3 +3,11 @@ module Data.Result where
 
 data Result e a = Result a | Error [e]
   deriving (Eq, Functor, Show)
+
+
+-- Instances
+
+instance Applicative (Result e) where
+  pure = Result
+  Error s <*> _ = Error s
+  Result f <*> a = fmap f a
