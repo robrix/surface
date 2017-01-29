57ae935f6be7b2a2664a9c6721222601125ce6a0
Rob Rix <rob.rix@me.com>
2017-01-29 11:15:34 -0500

    Add an Expr type.

--- src/Expr.hs
+++ src/Expr.hs
@@ -1,1 +1,41 @@
+{-# LANGUAGE DeriveFoldable, DeriveFunctor, GADTs #-}
 module Expr where
+
+import Data.Functor.Foldable
+
+data ExprF a where
+  Product :: a -> a -> ExprF a
+  Sum :: a -> a -> ExprF a
+  Function :: a -> a -> ExprF a
+  UnitT :: ExprF a
+  TypeT :: ExprF a
+
+  Abs :: Name -> a -> ExprF a
+  Var :: Name -> ExprF a
+  App :: a -> a -> ExprF a
+
+  InL :: a -> ExprF a
+  InR :: a -> ExprF a
+  Case :: a -> a -> a -> ExprF a
+
+  Pair :: a -> a -> ExprF a
+  Fst :: a -> ExprF a
+  Snd :: a -> ExprF a
+
+  Unit :: ExprF a
+  deriving (Eq, Foldable, Functor, Show)
+
+type Expr = Fix ExprF
+
+type TypeF = ExprF
+type Type = Fix TypeF
+
+type TermF = ExprF
+type Term = Fix TermF
+
+
+newtype Name = Name Integer
+  deriving (Eq, Show)
+
+unName :: Name -> Integer
+unName (Name n) = n
