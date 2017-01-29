282d438f88cd7aa77565f7921dad14efe219e181
Rob Rix <rob.rix@me.com>
2017-01-29 11:39:38 -0500

    Define an Alternative instance for Result.

--- src/Data/Result.hs
+++ src/Data/Result.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE DeriveFunctor #-}
 module Data.Result where
 
+import Control.Applicative
+
 data Result e a = Result a | Error [e]
   deriving (Eq, Functor, Show)
 
@@ -16,3 +18,9 @@ instance Monad (Result e) where
   return = pure
   Error s >>= _ = Error s
   Result a >>= f = f a
+
+instance Alternative (Result e) where
+  empty = Error []
+  Error s1 <|> Error s2 = Error (s1 ++ s2)
+  Result a <|> _ = Result a
+  _ <|> Result b = Result b
