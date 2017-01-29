cb1dcd1561c379bff77f2d030e15f859f7c88e26
Rob Rix <rob.rix@me.com>
2017-01-29 11:21:50 -0500

    Add a convenience for pretty-printing in the REPL.

--- src/Text/Pretty.hs
+++ src/Text/Pretty.hs
@@ -6,6 +6,9 @@ class Pretty t where
 class Functor f => Pretty1 f where
   prettyPrec1 :: f (Int, ShowS) -> (Int, ShowS)
 
+prettyPrint :: Pretty a => a -> IO ()
+prettyPrint = putStrLn . pretty
+
 prettyParen :: Int -> (Int, ShowS) -> ShowS
 prettyParen d (p, s) = showParen (p >= d) s
 
