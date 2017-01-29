2e7fff13b5d31d2ecbcf9af4248a2ccd242a3e87
Rob Rix <rob.rix@me.com>
2017-01-29 11:40:58 -0500

    Define a Judgement type.

--- src/Judgement.hs
+++ src/Judgement.hs
@@ -1,1 +1,12 @@
+{-# LANGUAGE GADTs #-}
 module Judgement where
+
+import Expr
+
+data Judgement a where
+  Check :: Term -> Type -> Judgement ()
+  Infer :: Term -> Judgement Type
+
+  IsType :: Term -> Judgement ()
+
+  Hole :: Judgement Type
