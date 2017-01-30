26b41dcd0872531b43512b9976bdffc473d284e8
Rob Rix <rob.rix@me.com>
2017-01-30 00:23:08 -0500

    Specialize Result to String again.

--- src/Data/Result.hs
+++ src/Data/Result.hs
@@ -4,28 +4,28 @@ module Data.Result where
 import Control.Applicative
 import Text.Pretty
 
-data Result e a = Result a | Error [e]
+data Result a = Result a | Error [String]
   deriving (Eq, Functor, Show)
 
 
 -- Instances
 
-instance Applicative (Result e) where
+instance Applicative Result where
   pure = Result
   Error s <*> _ = Error s
   Result f <*> a = fmap f a
 
-instance Monad (Result e) where
+instance Monad Result where
   return = pure
   Error s >>= _ = Error s
   Result a >>= f = f a
 
-instance Alternative (Result e) where
+instance Alternative Result where
   empty = Error []
   Error s1 <|> Error s2 = Error (s1 ++ s2)
   Result a <|> _ = Result a
   _ <|> Result b = Result b
 
-instance (Pretty e, Pretty a) => Pretty (Result e a) where
+instance Pretty a => Pretty (Result a) where
   prettyPrec (Result a) = prettyPrec a
-  prettyPrec (Error errors) = (0, foldr (.) id (fmap (\ e -> snd (prettyPrec e) . showChar '\n') errors))
+  prettyPrec (Error errors) = (0, foldr (.) id (fmap (\ e -> showString e . showChar '\n') errors))
