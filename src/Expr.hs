43884ef9d10b884d1fe39de719c3fd31a3d09694
Rob Rix <rob.rix@me.com>
2017-02-01 20:38:42 -0500

    Move showsTernaryWith to the top level.

--- src/Expr.hs
+++ src/Expr.hs
@@ -174,7 +174,7 @@ instance Show1 ExprF where
     UnitT -> showString "UnitT"
     Unit -> showString "Unit"
     TypeT -> showString "TypeT"
-    where
-      showsTernaryWith :: (Int -> a -> ShowS) -> (Int -> b -> ShowS) -> (Int -> c -> ShowS) -> String -> Int -> a -> b -> c -> ShowS
-      showsTernaryWith sp1 sp2 sp3 name d x y z = showParen (d > 10) $
-        showString name . showChar ' ' . sp1 11 x . showChar ' ' . sp2 11 y . showChar ' ' . sp3 11 z
+
+showsTernaryWith :: (Int -> a -> ShowS) -> (Int -> b -> ShowS) -> (Int -> c -> ShowS) -> String -> Int -> a -> b -> c -> ShowS
+showsTernaryWith sp1 sp2 sp3 name d x y z = showParen (d > 10) $
+  showString name . showChar ' ' . sp1 11 x . showChar ' ' . sp2 11 y . showChar ' ' . sp3 11 z
