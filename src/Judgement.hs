14ea0080037647b3b19f52f6c3cf95c516c347eb
Rob Rix <rob.rix@me.com>
2017-02-05 18:07:56 -0500

    Always produce the initial step.

--- src/Judgement.hs
+++ src/Judgement.hs
@@ -360,9 +360,9 @@ run context proof = case runStep context proof of
   Right next -> uncurry run next
 
 runSteps :: (Name, Context) -> Proof a -> [Either (Result a) ((Name, Context), Proof a)]
-runSteps context proof = case runStep context proof of
+runSteps context proof = Right (context, proof) : case runStep context proof of
   Left result -> [ Left result ]
-  Right next -> Right next : uncurry runSteps next
+  Right next -> uncurry runSteps next
 
 runStep :: (Name, Context) -> Proof a -> Either (Result a) ((Name, Context), Proof a)
 runStep context proof = case runFreer proof of
