af52ad62900806dba17eaa13e757d6aa86116b6b
Rob Rix <rob.rix@me.com>
2017-02-07 08:57:44 -0500

    Derive some instances.

--- src/Module.hs
+++ src/Module.hs
@@ -5,8 +5,10 @@ import Expr
 data Module = Module
   { moduleName :: String
   , moduleDeclarations :: [Declaration] }
+  deriving (Eq, Show)
 
 data Declaration = Declaration
   { declarationName :: String
   , declarationType :: Type
   , declarationTerm :: Term }
+  deriving (Eq, Show)
