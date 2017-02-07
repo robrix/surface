455584678b5d60d0094776a4a013c5ff4a5cdc2d
Rob Rix <rob.rix@me.com>
2017-02-07 09:02:05 -0500

    Pretty-print Modules.

--- src/Module.hs
+++ src/Module.hs
@@ -22,3 +22,9 @@ instance Pretty Declaration where
   prettyPrec _ Declaration{..}
     = showString declarationName . showString " : " . prettyPrec 0 declarationType . showChar '\n'
     . showString declarationName . showString " = " . prettyPrec 0 declarationTerm . showChar '\n'
+
+instance Pretty Module where
+  prettyPrec _ Module{..}
+    = showString "module " . showString moduleName . showString " where" . showChar '\n'
+    . showChar '\n'
+    . foldr (\ each into -> prettyPrec 0 each . showChar '\n' . into ) id moduleDeclarations
