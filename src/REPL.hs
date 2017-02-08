1905892c5ded43e2cbf450f25af9cd89d696cf68
Rob Rix <rob.rix@me.com>
2017-02-08 09:01:25 -0500

    Add a simple interpreter for the REPL.

--- src/REPL.hs
+++ src/REPL.hs
@@ -18,3 +18,13 @@ result s = Result s `andThen` return
 
 andThen :: f x -> (x -> Freer f a) -> Freer f a
 andThen = (Freer .) . flip Free
+
+
+runREPL :: REPL a -> IO a
+runREPL = iterFreer alg . fmap pure
+  where alg :: (x -> IO a) -> REPLF x -> IO a
+        alg cont repl = case repl of
+          Prompt s -> do
+            putStr s
+            getLine >>= cont
+          Result s -> putStrLn s >>= cont
