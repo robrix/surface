d58c4086df07c980f2e0df7d46450fb550b36917
Rob Rix <rob.rix@me.com>
2017-02-11 17:43:32 -0500

    Test that lambdas can take multiple params.

--- test/Parser/Spec.hs
+++ test/Parser/Spec.hs
@@ -20,3 +20,6 @@ spec = do
   describe "lambda" $ do
     it "can take single params" $
       lambda `parseString` "\\ a . a" `shouldBe` Result (makeLambda (N "a") (Expr.var (N "a")))
+
+    it "can take multiple params" $
+      lambda `parseString` "\\ a b . a b" `shouldBe` Result (makeLambda (N "a") (makeLambda (N "b") (Expr.var (N "a") # Expr.var (N "b"))))
