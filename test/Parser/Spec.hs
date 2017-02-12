1bfd56a465ca9b4c03fba8c0e1b0ba65c6945136
Rob Rix <rob.rix@me.com>
2017-02-12 12:38:45 -0500

    Qualify the parser names.

--- test/Parser/Spec.hs
+++ test/Parser/Spec.hs
@@ -2,28 +2,29 @@ module Parser.Spec where
 
 import Data.Result
 import Expr
-import Parser
+import Parser (parseString)
+import qualified Parser
 import Test.Hspec
 
 spec :: Spec
 spec = do
   describe "expr" $ do
     it "parses function types" $
-      expr `parseString` "Unit -> Unit" `shouldBe` Result (unitT .->. unitT)
+      Parser.expr `parseString` "Unit -> Unit" `shouldBe` Result (unitT .->. unitT)
 
     it "parses product types" $
-      expr `parseString` "Unit * Unit" `shouldBe` Result (unitT .*. unitT)
+      Parser.expr `parseString` "Unit * Unit" `shouldBe` Result (unitT .*. unitT)
 
     it "parses sum types" $
-      expr `parseString` "Unit + Unit" `shouldBe` Result (unitT .+. unitT)
+      Parser.expr `parseString` "Unit + Unit" `shouldBe` Result (unitT .+. unitT)
 
   describe "functionType" $ do
     it "associates to the right" $
-      functionType `parseString` "a -> b -> c" `shouldBe` functionType `parseString` "a -> (b -> c)"
+      Parser.functionType `parseString` "a -> b -> c" `shouldBe` Parser.functionType `parseString` "a -> (b -> c)"
 
   describe "lambda" $ do
     it "can take single params" $
-      lambda `parseString` "\\ a . a" `shouldBe` Result (makeLambda (N "a") (Expr.var (N "a")))
+      Parser.lambda `parseString` "\\ a . a" `shouldBe` Result (makeLambda (N "a") (Expr.var (N "a")))
 
     it "can take multiple params" $
-      lambda `parseString` "\\ a b . a b" `shouldBe` Result (makeLambda (N "a") (makeLambda (N "b") (Expr.var (N "a") # Expr.var (N "b"))))
+      Parser.lambda `parseString` "\\ a b . a b" `shouldBe` Result (makeLambda (N "a") (makeLambda (N "b") (var (N "a") # var (N "b"))))
