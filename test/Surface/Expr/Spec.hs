8ff24612e2bf486b8753a99519c5a41ced8f4035
Rob Rix <rob.rix@me.com>
2017-02-12 13:57:17 -0500

    Test that renaming bound variables doesn’t.

--- test/Surface/Expr/Spec.hs
+++ test/Surface/Expr/Spec.hs
@@ -8,3 +8,6 @@ spec = do
   describe "rename" $ do
     it "renames free variables" $
       rename (N "a") (N "b") (makeLambda (N "c") (varN "a")) `shouldBe` makeLambda (N "c") (varN "b")
+
+    it "does not rename bound variables" $
+      rename (N "a") (N "b") (makeLambda (N "a") (varN "a")) `shouldBe` makeLambda (N "a") (varN "a")
