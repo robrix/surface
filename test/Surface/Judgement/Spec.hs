537822e52902b403e3f088a3a5b959febb412964
Rob Rix <rob.rix@me.com>
2017-02-12 21:28:48 -0500

    Test that judgemental equality is symmetric.

--- test/Surface/Judgement/Spec.hs
+++ test/Surface/Judgement/Spec.hs
@@ -1,6 +1,7 @@
 module Surface.Judgement.Spec where
 
 import Data.Functor.Listable
+import Data.Result
 import Judgement
 import Test.Hspec
 import Test.Hspec.LeanCheck
@@ -10,3 +11,9 @@ spec = do
   describe "equals" $ do
     prop "reflexivity" . forAll embedTiers $
       \ expr -> run (expr `equals` expr) `shouldBe` return ()
+
+    prop "symmetry" . forAll (embedTiers >< embedTiers) . uncurry $
+      \ a b -> eraseErrors (run (a `equals` b)) `shouldBe` eraseErrors (run (b `equals` a))
+
+eraseErrors :: Result a -> Result a
+eraseErrors = mapErrors (const [])
