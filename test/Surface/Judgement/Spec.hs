3c59e43e190720882e38eaf32d6d9531c683969d
Rob Rix <rob.rix@me.com>
2017-02-12 21:20:22 -0500

    Test the reflexivity of the equality judgement.

--- test/Surface/Judgement/Spec.hs
+++ test/Surface/Judgement/Spec.hs
@@ -1,6 +1,12 @@
 module Surface.Judgement.Spec where
 
+import Data.Functor.Listable
+import Judgement
 import Test.Hspec
+import Test.Hspec.LeanCheck
 
 spec :: Spec
-spec = return ()
+spec = do
+  describe "equals" $ do
+    prop "reflexivity" . forAll embedTiers $
+      \ expr -> run (expr `equals` expr) `shouldBe` return ()
