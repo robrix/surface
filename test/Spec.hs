3d64253c2e05242d7b2a52db05e484d724fb023b
Rob Rix <rob.rix@me.com>
2017-02-14 18:01:01 -0500

    Set the bound in code.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,12 @@
-import Test.Hspec
 import Parser.Spec as Parser
 import Surface.Expr.Spec as Expr
 import Surface.Judgement.Spec as Judgement
+import Test.Hspec
+import Test.Hspec.Core.Runner
 
 main :: IO ()
-main = hspec . parallel $ do
+main = hspecWith config . parallel $ do
   describe "Expr" Expr.spec
   describe "Judgement" Judgement.spec
   describe "Parser" Parser.spec
+  where config = defaultConfig { configSmallCheckDepth = 1000 }
