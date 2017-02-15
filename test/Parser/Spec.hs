be65be5ca1009b6ad59e3229151ba329e355c057
Rob Rix <rob.rix@me.com>
2017-02-14 22:26:57 -0500

    Test against the whole string.

--- test/Parser/Spec.hs
+++ test/Parser/Spec.hs
@@ -10,41 +10,41 @@ spec :: Spec
 spec = do
   describe "expr" $ do
     it "parses function types" $
-      Parser.expr `parseString` "Unit -> Unit" `shouldBe` Result (unitT .->. unitT)
+      whole expr `parseString` "Unit -> Unit" `shouldBe` Result (unitT .->. unitT)
 
     it "parses product types" $
-      Parser.expr `parseString` "Unit * Unit" `shouldBe` Result (unitT .*. unitT)
+      whole expr `parseString` "Unit * Unit" `shouldBe` Result (unitT .*. unitT)
 
     it "parses sum types" $
-      Parser.expr `parseString` "Unit + Unit" `shouldBe` Result (unitT .+. unitT)
+      whole expr `parseString` "Unit + Unit" `shouldBe` Result (unitT .+. unitT)
 
   describe "functionType" $ do
     it "parses right-associatively" $
-      Parser.functionType `parseString` "a -> b -> c" `shouldBe` Parser.functionType `parseString` "a -> (b -> c)"
+      whole functionType `parseString` "a -> b -> c" `shouldBe` Parser.functionType `parseString` "a -> (b -> c)"
 
     it "constructs right-associated functions" $
-      Parser.functionType `parseString` "a -> b -> c" `shouldBe` Result (varN "a" .->. (varN "b" .->. varN "c"))
+      whole functionType `parseString` "a -> b -> c" `shouldBe` Result (varN "a" .->. (varN "b" .->. varN "c"))
 
     it "can take type applications" $
-      Parser.functionType `parseString` "a b -> c" `shouldBe` Result (varN "a" # varN "b" .->. varN "c")
+      whole functionType `parseString` "a b -> c" `shouldBe` Result (varN "a" # varN "b" .->. varN "c")
 
     it "can return type applications" $
-      Parser.functionType `parseString` "a -> b c" `shouldBe` Result (varN "a" .->. varN "b" # varN "c")
+      whole functionType `parseString` "a -> b c" `shouldBe` Result (varN "a" .->. varN "b" # varN "c")
 
   describe "lambda" $ do
     it "can take single params" $
-      Parser.lambda `parseString` "\\ a . a" `shouldBe` Result (makeLambda (N "a") (varN "a"))
+      whole lambda `parseString` "\\ a . a" `shouldBe` Result (makeLambda (N "a") (varN "a"))
 
     it "can take multiple params" $
-      Parser.lambda `parseString` "\\ a b . a b" `shouldBe` Result (makeLambda (N "a") (makeLambda (N "b") (varN "a" # varN "b")))
+      whole lambda `parseString` "\\ a b . a b" `shouldBe` Result (makeLambda (N "a") (makeLambda (N "b") (varN "a" # varN "b")))
 
   describe "pi" $ do
     it "binds a variable" $
-      Parser.piType `parseString` "(a : Type) -> a -> a" `shouldBe` Result (makePi (N "a") typeT (varN "a" .->. varN "a"))
+      whole piType `parseString` "(a : Type) -> a -> a" `shouldBe` Result (makePi (N "a") typeT (varN "a" .->. varN "a"))
 
     it "can occur in the body of lambdas" $
-      Parser.lambda `parseString` "\\ p q. (c : Type) -> (p -> q -> c) -> c" `shouldBe` Result (makeLambda (N "p") (makeLambda (N "q") (makePi (N "c") typeT ((varN "p" .->. varN "q" .->. varN "c") .->. varN "c"))))
+      whole lambda `parseString` "\\ p q. (c : Type) -> (p -> q -> c) -> c" `shouldBe` Result (makeLambda (N "p") (makeLambda (N "q") (makePi (N "c") typeT ((varN "p" .->. varN "q" .->. varN "c") .->. varN "c"))))
 
   describe "declaration" $ do
     it "parses a type and value" $
-      Parser.declaration `parseString` "and : Type -> Type -> Type\nand = \\p q. (c : Type) -> (p -> q -> c) -> c" `shouldBe` Result (Declaration "and" (typeT .->. typeT .->. typeT) (makeLambda (N "p") (makeLambda (N "q") (makePi (N "c") typeT ((varN "p" .->. varN "q" .->. varN "c") .->. varN "c")))))
+      whole declaration `parseString` "and : Type -> Type -> Type\nand = \\p q. (c : Type) -> (p -> q -> c) -> c" `shouldBe` Result (Declaration "and" (typeT .->. typeT .->. typeT) (makeLambda (N "p") (makeLambda (N "q") (makePi (N "c") typeT ((varN "p" .->. varN "q" .->. varN "c") .->. varN "c")))))
