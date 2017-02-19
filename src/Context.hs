d1c4e89ee1bea002df2b9563dd7ff52d998e4efb
Rob Rix <rob.rix@me.com>
2017-02-19 10:07:43 -0500

    :fire: Schm & Index.

--- src/Context.hs
+++ src/Context.hs
@@ -2,7 +2,6 @@
 module Context where
 
 import Data.Foldable (toList)
-import Data.Functor.Classes
 import Expr
 import Text.Pretty
 
@@ -40,18 +39,6 @@ data Extension = Restore | Replace Suffix
   deriving (Eq, Show)
 
 
-data Index a = Z | S a
-  deriving (Eq, Foldable, Functor, Show)
-
-data Schm a
-  = Type Type
-  | All (Schm (Index a))
-  | LetS Type (Schm (Index a))
-  deriving (Eq, Foldable, Functor, Show)
-
-type Scheme = Schm Name
-
-
 -- Instances
 
 instance Pretty1 Backward where
@@ -67,12 +54,3 @@ instance Pretty Binding where
 
 instance Pretty TermEntry where
   prettyPrec d (name ::: scheme) = showParen (d > 9) $ prettyPrec 10 name . showString " :: " . prettyPrec 10 scheme
-
-instance Pretty1 Schm where
-  liftPrettyPrec _ _ d (Type ty) = prettyPrec d ty
-  liftPrettyPrec pp pl d (All schm) = showsUnaryWith (liftPrettyPrec (liftPrettyPrec pp pl) (liftPrettyList pp pl)) "All" d schm
-  liftPrettyPrec pp pl d (LetS ty schm) = showsBinaryWith prettyPrec (liftPrettyPrec (liftPrettyPrec pp pl) (liftPrettyList pp pl)) "LetS" d ty schm
-
-instance Pretty1 Index where
-  liftPrettyPrec _ _ _ Z = id
-  liftPrettyPrec pp _ d (S a) = pp d a . showChar '\''
