fa09d23ffce0c23e76978d52660d631fb39220d0
Rob Rix <rob.rix@me.com>
2017-07-29 20:01:32 -0400

    Define a Num instance for Erasure.

--- src/Surface/Erasure.hs
+++ src/Surface/Erasure.hs
@@ -1,4 +1,26 @@
 module Surface.Erasure where
 
+import Control.Exception
+
 data Erasure = Erased | Present
   deriving (Eq, Ord, Show)
+
+instance Num Erasure where
+  Erased + a = a
+  a + Erased = a
+  _ + _ = Present
+
+  Present * a = a
+  a * Present = a
+  _ * _ = Erased
+
+  abs = id
+
+  signum = id
+
+  negate Erased = Erased
+  negate Present = throw Underflow
+
+  fromInteger n | n == 0 = Erased
+                | n == 1 = Present
+                | otherwise = throw Underflow
