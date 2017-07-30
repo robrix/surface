5cf0a8e3034f6ba3e9556f2abe83d23b24741478
Rob Rix <rob.rix@me.com>
2017-07-29 20:37:59 -0400

    Rename Multiple to More.

--- src/Surface/Usage.hs
+++ src/Surface/Usage.hs
@@ -3,14 +3,14 @@ module Surface.Usage where
 import Data.Semigroup
 import Data.Semiring
 
-data Usage = Zero | One | Multiple
+data Usage = Zero | One | More
   deriving (Bounded, Enum, Eq, Ord, Show)
 
 
 instance Semigroup Usage where
   Zero <> a = a
   a <> Zero = a
-  _ <> _ = Multiple
+  _ <> _ = More
 
 instance Monoid Usage where
   mempty = Zero
@@ -21,4 +21,4 @@ instance Semiring Usage where
   Zero >< _ = Zero
   _ >< Zero = Zero
   One >< One = One
-  _ >< _ = Multiple
+  _ >< _ = More
