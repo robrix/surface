c2bebe15fb5f2669af9614152776d6bd5d8a3de4
Rob Rix <rob.rix@me.com>
2017-07-29 20:23:14 -0400

    Define a Semiring over Nums.

--- src/Data/Semiring.hs
+++ src/Data/Semiring.hs
@@ -1,5 +1,6 @@
 module Data.Semiring where
 
+import Data.Function
 import Data.Semigroup
 
 zero :: Monoid m => m
@@ -8,3 +9,17 @@ zero = mempty
 class (Semigroup m, Monoid m) => Semiring m where
   one :: m
   (><) :: m -> m -> m
+
+
+newtype NumRing n = NumRing { unNumRing :: n }
+
+instance Num n => Semigroup (NumRing n) where
+  (<>) = (NumRing .) . ((+) `on` unNumRing)
+
+instance Num n => Monoid (NumRing n) where
+  mempty = NumRing 0
+  mappend = (<>)
+
+instance Num n => Semiring (NumRing n) where
+  one = NumRing 1
+  (><) = (NumRing .) . ((*) `on` unNumRing)
