921ec0bf91250fd2f577880a69abac9323bada6d
Rob Rix <rob.rix@me.com>
2017-07-29 20:36:16 -0400

    :fire: the Num instance for Erasure.

--- src/Surface/Erasure.hs
+++ src/Surface/Erasure.hs
@@ -1,39 +1,23 @@
 module Surface.Erasure where
 
-import Control.Exception
 import Data.Semigroup
 import Data.Semiring
 
 data Erasure = Erased | Present
   deriving (Bounded, Enum, Eq, Ord, Show)
 
-instance Num Erasure where
-  Erased + a = a
-  a + Erased = a
-  _ + _ = Present
-
-  Present * a = a
-  a * Present = a
-  _ * _ = Erased
-
-  abs = id
-
-  signum = id
-
-  negate Erased = Erased
-  negate Present = throw Underflow
-
-  fromInteger n | n == 0 = Erased
-                | n == 1 = Present
-                | otherwise = throw Underflow
 
 instance Semigroup Erasure where
-  (<>) = (+)
+  Erased <> a = a
+  a <> Erased = a
+  _ <> _ = Present
 
 instance Monoid Erasure where
   mempty = Erased
-  mappend = (+)
+  mappend = (<>)
 
 instance Semiring Erasure where
   one = Present
-  (><) = (*)
+  Present >< a = a
+  a >< Present = a
+  _ >< _ = Erased
