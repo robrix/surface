3b817a9d82ddd763e267dada8b08cd8e0314035d
Rob Rix <rob.rix@me.com>
2017-07-29 20:37:12 -0400

    :fire: the Num instance for Usage.

--- src/Surface/Usage.hs
+++ src/Surface/Usage.hs
@@ -1,42 +1,24 @@
 module Surface.Usage where
 
-import Control.Exception
 import Data.Semigroup
 import Data.Semiring
 
 data Usage = Zero | One | Multiple
   deriving (Bounded, Enum, Eq, Ord, Show)
 
-instance Num Usage where
-  Zero + a = a
-  a + Zero = a
-  _ + _ = Multiple
-
-  Zero * _ = Zero
-  _ * Zero = Zero
-  One * One = One
-  _ * _ = Multiple
-
-  abs = id
-
-  signum Zero = Zero
-  signum _ = One
-
-  negate Zero = Zero
-  negate _ = throw Underflow
-
-  fromInteger n | n < 0 = throw Underflow
-                | n == 0 = Zero
-                | n == 1 = One
-                | otherwise = Multiple
 
 instance Semigroup Usage where
-  (<>) = (+)
+  Zero <> a = a
+  a <> Zero = a
+  _ <> _ = Multiple
 
 instance Monoid Usage where
   mempty = Zero
-  mappend = (+)
+  mappend = (<>)
 
 instance Semiring Usage where
   one = One
-  (><) = (*)
+  Zero >< _ = Zero
+  _ >< Zero = Zero
+  One >< One = One
+  _ >< _ = Multiple
