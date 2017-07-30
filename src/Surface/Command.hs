c4daad87fbb81c9e7b2136686b5a6b296a6f2fdd
Rob Rix <rob.rix@me.com>
2017-07-30 09:56:01 -0400

    Dedent runCommand.

--- src/Surface/Command.hs
+++ src/Surface/Command.hs
@@ -13,19 +13,19 @@ data Command
 
 runCommand :: Command -> IO ()
 runCommand command = case command of
-    Interactive -> REPL.runREPL REPL.repl
-    Run path -> do
-      result <- parseFromFile source path
-      printResult $ do
-        modules <- result
-        for_ modules (runProof . checkModule)
-    Debug path -> do
-      result <- parseFromFile source path
-      traverse_ (traverse_ prettyPrint) $ do
-        modules <- result
-        return $ do
-          m <- toList modules
-          runSteps' (checkModule m :: Proof () ()) initialState
+  Interactive -> REPL.runREPL REPL.repl
+  Run path -> do
+    result <- parseFromFile source path
+    printResult $ do
+      modules <- result
+      for_ modules (runProof . checkModule)
+  Debug path -> do
+    result <- parseFromFile source path
+    traverse_ (traverse_ prettyPrint) $ do
+      modules <- result
+      return $ do
+        m <- toList modules
+        runSteps' (checkModule m :: Proof () ()) initialState
 
 printResult :: Pretty a => Either [String] a -> IO ()
 printResult result = case result of
